e42bcf724506c5206b5a5fee5fe3cb51f07a7829
mergd <24722014+mergd@users.noreply.github.com>
2026-09-10 11:40:01 -0700

    Bump iOS build to 17

--- app.config.ts
+++ app.config.ts
@@ -47,7 +47,7 @@ const config: ExpoConfig = {
     },
   },
   ios: {
-    buildNumber: '16',
+    buildNumber: '17',
     icon: './assets/images/icon.png',
     bundleIdentifier: 'dev.william.latch',
     supportsTablet: false,
